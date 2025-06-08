24167c9f99d7c01ceaa5756a768c78eb94b9ee9c
Your Name <you@example.com>
2025-06-09 07:59:37 +0800

    rename lesson 1, finished lesson 2, started lesson 3

---
+++ lesson-003/src/my_common_pkg/src/talker.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class Talker : public rclcpp::Node
+{
+public:
+  Talker()
+  : Node("talker")
+  {
+    // 创建 publisher，话题名 "chatter"，队列大小 10
+    publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
+
+    // 每 500ms 调用一次 timer_callback
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&Talker::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t count_ = 0;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Talker>());
+  rclcpp::shutdown();
+  return 0;
+}
