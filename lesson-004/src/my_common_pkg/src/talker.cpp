4b044a5ea0a734a93416c80a0b2f407902cef123
Your Name <you@example.com>
2025-08-31 12:31:25 +0800

    added lesson 4

---
+++ lesson-004/src/my_common_pkg/src/talker.cpp
@@ -0,0 +1,14 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world my_common_pkg package\n");
+  return 0;
+}
